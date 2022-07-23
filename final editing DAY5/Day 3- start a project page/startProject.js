f2e01363095b566ca4e4b4b8bdd9348c91fc05bd
Deekshachandan <deeksharajpurohit15@gmail.com>
2022-07-23 19:50:15 +0530

    final editing
    final editing to all the pages added

---
+++ final editing DAY5/Day 3- start a project page/startProject.js
@@ -0,0 +1,32 @@
+//adding functionality to the button
+document.getElementById("button1").addEventListener("click", firstfun)
+
+function firstfun(){
+    window.location.href="https://www.kickstarter.com/start?ref=learn_top"
+}
+
+
+
+
+
+
+
+
+//aading functionality to the second button
+document.getElementById("nine_button").addEventListener("click", start)
+
+
+
+function start(){
+
+    window.location.href="https://www.kickstarter.com/start?ref=learn_category_art"
+}
+
+
+
+//adding functionality to the button
+document.getElementById("eleven_button").addEventListener("click",myfun)
+
+function myfun(){
+    window.location.href="https://www.kickstarter.com/start?ref=learn_bottom"
+}
