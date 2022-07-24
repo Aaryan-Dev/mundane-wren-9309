c23a6a99db6c2efde57496fa4fc2d3a022735ed7
Aryan Dev <nobodygivesafak@gmail.com>
2022-07-24 11:44:27 +0530

    fw19_1172, fw19_0727, fp03_322, fw19_0506 final commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+document.querySelector("#feature_recommend").addEventListener("click", go);
+
+function go() {
+    window.location.href = "https://www.kickstarter.com/projects/maidawitherslegacy/legacy-50-years-dance-on-the-edge?ref=section-homepage-featured-project"
+
+}
